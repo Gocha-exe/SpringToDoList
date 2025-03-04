08e42e5118139c330821a5349a615f526f43cd78
Gocha-exe <mailformyworkjava@gmail.com>
2025-03-04 23:51:15 +0300

    1

---
+++ src/main/java/model/TodoItem.java
@@ -0,0 +1,12 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TodoItem {
+    git remote add origin
+    @Id
+    Long id;
+    String title;
+}
